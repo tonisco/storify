d94317c9dc302f3da3a030466ed88894ad605b23
tonisco <tonyumeozulu@gmail.com>
2023-08-20 17:22:45 +0100

    moved data to ts file

---
+++ data/cartegoriesImage.ts
@@ -0,0 +1,15 @@
+const categoriesImage = {
+  Dresses: require("../assets/images/dresses.jpg"),
+  Suits: require("../assets/images/suits.jpg"),
+  Jackets: require("../assets/images/jackets.jpg"),
+  Accessories: require("../assets/images/accessories.jpg"),
+  Activewear: require("../assets/images/active-wear.jpg"),
+  Shirts: require("../assets/images/shirts.jpg"),
+  Sweaters: require("../assets/images/sweaters.jpg"),
+  Shoes: require("../assets/images/shoes.jpg"),
+  Skirts: require("../assets/images/shirts.jpg"),
+  Loungewear: require("../assets/images/longe-wear.jpg"),
+  Shorts: require("../assets/images/accessories.jpg"),
+}
+
+export default categoriesImage
